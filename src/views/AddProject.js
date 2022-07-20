14db801d20b3d836fa9bc42864d27a7c203db995
Hasan <75369571+hasanhut@users.noreply.github.com>
2022-07-20 15:13:52 +0300

    Update AddProject.js

--- src/views/AddProject.js
+++ src/views/AddProject.js
@@ -1,121 +1,52 @@
 import React, { useState } from "react";
 import axios from "axios";
-const url = "https://localhost:7274/api/project";
+import { useNavigate } from 'react-router-dom';
+
+const url = "https://62d7dee29c8b5185c77dd58a.mockapi.io/project";
 
 function AddProject() {
-    const [projectName, setProjectName] = useState("");
-    const [projectExplanation, setProjectExplanation] = useState("");
-    const postData = () => {
+    const [name, setName] = useState("");
+    const [explanation, setExplanation] = useState("");
+    const postData = (e) => {
+        e.preventDefault();
         axios.post(url, {
-            projectName,
-            projectExplanation,
-        });
+            name,
+            explanation,
+        }).then((res) => console.log(res));
     };
     return (
-        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div>
-                    <img
-                        className="mx-auto h-12 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt="Workflow"
-                    />
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Create a TaskTree Project
-                    </h2>
-                </div>
-                <form className="mt-8 space-y-6" action="#" method="POST">
-                    <input type="hidden" name="remember" defaultValue="true" />
-                    <div className="rounded-md shadow-sm -space-y-px">
-                        <div>
-                            <label htmlFor="projectName" className="sr-only">
-                                Project Name
-                            </label>
-                            <input
-                                id="projectName"
-                                name="projectName"
-                                required
-                                className="appearance-none rounded-none relative block
-                  w-full px-3 py-2 border border-gray-300
-                  placeholder-gray-500 text-gray-900 rounded-t-md
-                  focus:outline-none focus:ring-indigo-500
-                  focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Project Name"
-                                onChange={(e) => setProjectName(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label
-                                htmlFor="projectExplanation"
-                                className="sr-only"
-                            >
-                                Project Explanation
-                            </label>
-                            <input
-                                id="projectExplanation"
-                                name="projectExplanation"
-                                required
-                                className="appearance-none rounded-none relative block
-                  w-full px-3 py-2 border border-gray-300
-                  placeholder-gray-500 text-gray-900 rounded-b-md
-                  focus:outline-none focus:ring-indigo-500
-                  focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Project Explanation"
-                                onChange={(e) =>
-                                    setProjectExplanation(e.target.value)
-                                }
-                            />
-                        </div>
-                        {/*<div>
-                            <label htmlFor="start-date" className="sr-only">
-                                Project Start-Date
-                            </label>
-                            <input
-                                id="start-date"
-                                name="start-date"
-                                required
-                                className="appearance-none rounded-none relative block
-                  w-full px-3 py-2 border border-gray-300
-                  placeholder-gray-500 text-gray-900 rounded-b-md
-                  focus:outline-none focus:ring-indigo-500
-                  focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Project Start-Date"
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="end-date" className="sr-only">
-                                Project End-Date
-                            </label>
-                            <input
-                                id="end-date"
-                                name="end-date"
-                                required
-                                className="appearance-none rounded-none relative block
-                  w-full px-3 py-2 border border-gray-300
-                  placeholder-gray-500 text-gray-900 rounded-b-md
-                  focus:outline-none focus:ring-indigo-500
-                  focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Project End-Date"
-                            />
-                        </div>*/}
-                    </div>
+        <>
+            <div className="bg-gray-700 min-h-screen flex items-center">
+                <div className="w-full">
+                    <h2 className="text-center text-blue-400 font-bold text-2xl uppercase mb-10">Create a new Project</h2>
+                    <div className="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
+                        <form action="">
+                            <div className="mb-5">
+                                <label htmlFor="name" className="block mb-2 font-bold text-gray-600">Project Name</label>
+                                <input type="text" id="name" name="name" onChange={(e) => setName(e.target.value)} placeholder="Put in your project name." className="border border-gray-300 shadow p-3 w-full rounded mb-" />
+                            </div>
 
-                    <div>
-                        <button
-                            type="submit"
-                            onClick={postData}
-                            className="group relative w-full flex justify-center
-                py-2 px-4 border border-transparent text-sm font-medium
-                rounded-md text-white bg-indigo-600 hover:bg-indigo-700
-                focus:outline-none focus:ring-2 focus:ring-offset-2
-                focus:ring-indigo-500"
-                        >
-                            Create Project
-                        </button>
+                            <div className="mb-5">
+                                <label htmlFor="explanation" className="block mb-2 font-bold text-gray-600">Project Explanation</label>
+                                <input type="text" id="explanation" name="explanation" onChange={(e) => setExplanation(e.target.value)} placeholder="Put in your project explanation." className="border border-gray-300 shadow p-3 w-full rounded mb-" />
+                            </div>
+                            <div className="mb-5">
+                                <label htmlFor="startDate" className="block mb-2 font-bold text-gray-600">Project Start Date</label>
+                                <input type="text" id="startDate" name="startDate" onChange={(e) => setExplanation(e.target.value)} placeholder="Put in your project start date." className="border border-gray-300 shadow p-3 w-full rounded mb-" />
+                            </div>
+                            <div className="mb-5">
+                                <label htmlFor="endDate" className="block mb-2 font-bold text-gray-600">Project End Date</label>
+                                <input type="text" id="endDate" name="endDate" onChange={(e) => setExplanation(e.target.value)} placeholder="Put in your project end date." className="border border-gray-300 shadow p-3 w-full rounded mb-" />
+                            </div>
+
+                            <button className="block w-full bg-blue-500 text-white font-bold p-4 rounded-lg" onClick={postData}>Submit</button>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
-        </div>
+
+
+        </>
     );
 }
 
